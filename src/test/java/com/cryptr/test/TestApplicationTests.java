f2da1e5e4f50b35e9ffa5fc55ff0e85af07f812d
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-03-09 15:26:28 +0100

    initial state from spring boot starter

---
+++ src/test/java/com/cryptr/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptr.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
